bd9b0ea7a47c622c7e7dad3067fd0696c6ddda15
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-07 13:20:17 +0200

    removed comment

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -2,7 +2,6 @@ import { abilities } from "../constants";
 
 const FeatureCards = () => {
   return (
-    //! Styling elements might not work rn
     <div className="w-full padding-x-lg">
       <div className="mx-auto grid-3-cols">
         {abilities.map(({ imgPath, title, desc }) => (
