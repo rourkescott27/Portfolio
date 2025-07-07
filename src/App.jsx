29aac9a11472a0eb5c709cbf686f64e5abc300aa
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-07 13:19:06 +0200

    pt9
    added feature cards to show personal strengths and qualities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import LogoSection from "./components/LogoSection.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCards />
     </>
   );
 };
